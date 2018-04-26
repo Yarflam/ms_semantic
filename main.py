11d6e3ebecd9d7a3e4c25380044b6fdca114516e
Yarflam <iarflam@gmail.com>
2018-04-26 17:42:42 +0200

    Output Python + loader tfhub container

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import tensorflow_hub as hub
 import matplotlib.pyplot as plt
 import mysql.connector as mariadb
 from pymongo import MongoClient
-from bson.json_util import dumps
 from bson.objectid import ObjectId
 import numpy as np
 import json as JSON
@@ -25,19 +24,10 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # Tools #
 #########
 
-def cleanUpUnicode (text):
-	matches = re.finditer("\\\\u([a-z0-9]{2})([a-z0-9]{2})", text)
-	for match in matches:
-		cdec = int(match.group(2), 16)
-		if cdec >= 128:
-			chex = hex(cdec % 128)[2:].zfill(2)
-			text = text.replace(match.group(0), '')
-	return text
-
-def getKeywords (code):
+def getExtractWords (code):
 	code = re.sub('<script[^>]+>[^<]+</script>', ' ', code, re.IGNORECASE)
 	code = re.sub('<[^>]+>', ' ', code)
-	code = re.sub('[\\n\\r\\s ]+', ' ', code)
+	code = re.sub('[\\n\\r\\s;,: -]+', ' ', code)
 	return code
 
 # Configuration
@@ -84,33 +74,42 @@ def getLastSpiderResults (mongodb_client, n=10):
 	output = []
 	cursor = mongodb_client.admin.spider_results.find({'ms_semantic': False}).limit(n)
 	for doc in cursor:
-		output.append(JSON.loads(dumps(doc)))
+		output.append(doc)
 	return output
 
 ############
 # Main App #
 ############
 def main ():
+	# Sets
+	tfs_name, tfs_version = 'nnlm-en-dim50', 1
+
 	# Get the configuration
 	config = getConfig()
 
 	# Connect to MariaDb
+	print('* Connecting to MariaDb')
 	# mysql_client = getClientMariaDb(config['mysql'])
 
 	# Connect to MongoDb
-	mongodb_client = getClientMongoDb(config['mongodb'])
+	print('* Connecting to MongoDb')
+	# mongodb_client = getClientMongoDb(config['mongodb'])
 
-	#
+	# Initialize Tensorflow Hub
+	print('* Load TensorflowHub.%s' % tfs_name)
+	embed = hub.Module("https://tfhub.dev/google/%s/%s" % (tfs_name, tfs_version))
+	model = embed(["mozart"])
+	print(model)
+	return True
+
 	# Begin the treatment
-	#
+	print('* Get the last documents')
 	items = getLastSpiderResults(mongodb_client, 1)
 	if len(items):
 		# Get the fist document
 		html = items[0]['html']
-		# Get the keywords
-		keywords = getKeywords(html)
-		# test = cleanUpUnicode(keywords)
-		print(keywords)
+		# Get the words
+		words = getExtractWords(html)
 
 	print('End')
 
