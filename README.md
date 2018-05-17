0aec560df16df9b67a9e3db629801992275aa8bc
Yarflam <iarflam@gmail.com>
2018-05-17 09:06:31 +0200

    First tests with Tensorflow Hub

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # ms_semantic
-Micro Service - Semantic Analyzer for the project SEO
+
+Middle Service -  Semantic Analyzer for the project SEO.
+
+## Prerequired
+
+You need a Docker environment to use it and two databases, MongoDB and MySQL.
+
+- Mongodb is the hot base : the spider save the pages, you have a wonderfull datalack with to work.
+
+- MySQL is the cold base : the script save the results, the front can show the data as they like.
+
+# How to configure ?
+
+Copy the file example.config.json and rename it as "config.json".
+
+In the config.json, you have to add the credential informations for the databases.
+
+## How to run the service ?
+
+Execute on your command line :
+
+> $ docker-compose up -d
+
+It's run !
+
+## How can I improve it ?
+
+Simple, you have to remove the endpoint in the docker-compose file and restart it.
+
+If you wan't to debugging the python script :
+
+> $ docker exec pysemantic bash /home/pysemantic.sh
+
+## How can I offer you a coffee ?
+
+It's sweet. Thank you so much !
+
+But I don't have any bitcoin address to available now. I will be back ...
--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 ################
 # import seaborn as sns
 # import pandas as pd
+import word2vec
 import tensorflow as tf
 import tensorflow_hub as hub
 import matplotlib.pyplot as plt
@@ -20,99 +21,133 @@ CONFIG_FILE = "config.json"
 # TF ignore the standards errors
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+# Current path
+dir_path = os.path.dirname(os.path.realpath(__file__))+os.sep
+
 #########
 # Tools #
 #########
 
 def getExtractWords (code):
-	code = re.sub('<script[^>]+>[^<]+</script>', ' ', code, re.IGNORECASE)
-	code = re.sub('<[^>]+>', ' ', code)
-	code = re.sub('[\\n\\r\\s;,: -]+', ' ', code)
-	return code
+    code = re.sub('<script[^>]+>[^<]+</script>', ' ', code, re.IGNORECASE)
+    code = re.sub('<[^>]+>', ' ', code)
+    code = re.sub('[\\n\\r\\s;,: -]+', ' ', code)
+    return code
 
 # Configuration
 def getConfig ():
-	# Open the config file
-	with open(CONFIG_FILE, 'r') as file:
-		# Read and extract it
-		data = file.read()
-		json = JSON.loads(data)
-	return json
+    # Check the config file
+    if os.path.isfile(dir_path+CONFIG_FILE):
+        # Open the config file
+        with open(dir_path+CONFIG_FILE, 'r') as file:
+            # Read and extract it
+            data = file.read()
+            json = JSON.loads(data)
+        return json
+    else:
+        print('Warning: the configuration file %s doesn\'t exists.'
+            % (dir_path+CONFIG_FILE))
+        quit()
 
 ##############
 # DB Clients #
 ##############
 
 def getClientMariaDb (args):
-	return mariadb.connect(
-		host = args['host'],
-		port = args['port'],
-		user = args['user'],
-		password = args['pass'],
-		database = args['base'])
+    return mariadb.connect(
+        host = args['host'],
+        port = args['port'],
+        user = args['user'],
+        password = args['pass'],
+        database = args['base'])
 
 def getClientMongoDb (args):
-	return MongoClient('mongodb://%s:%s@%s:%s/%s' % (
-		args['user'], args['pass'], args['host'],
-		args['port'], args['base']
-	))
+    return MongoClient('mongodb://%s:%s@%s:%s/%s' % (
+        args['user'], args['pass'], args['host'],
+        args['port'], args['base']
+    ))
 
 ##############
 # DB Request #
 ##############
 
 def queryMariaDb (mysql_client, request):
-	dbHandle = mysql_client.cursor()
-	dbHandle.execute(request)
-	return dbHandle.fetchall()
+    dbHandle = mysql_client.cursor()
+    dbHandle.execute(request)
+    return dbHandle.fetchall()
 
 ##########################
 # Specific Data Endpoint #
 ##########################
 
 def getLastSpiderResults (mongodb_client, n=10):
-	output = []
-	cursor = mongodb_client.admin.spider_results.find({'ms_semantic': False}).limit(n)
-	for doc in cursor:
-		output.append(doc)
-	return output
+    output = []
+    cursor = mongodb_client.admin.spider_results.find({'ms_semantic': False}).limit(n)
+    for doc in cursor:
+        output.append(doc)
+    return output
 
 ############
 # Main App #
 ############
 def main ():
-	# Sets
-	tfs_name, tfs_version = 'nnlm-en-dim50', 1
+    # Sets
+    tfs_name, tfs_version = 'nnlm-en-dim128', 1
+    # tfs_name = 'universal-sentence-encoder'
 
-	# Get the configuration
-	config = getConfig()
+    # Get the configuration
+    config = getConfig()
 
-	# Connect to MariaDb
-	print('* Connecting to MariaDb')
-	# mysql_client = getClientMariaDb(config['mysql'])
+    # Connect to MariaDb
+    print('* Connecting to MariaDb')
+    # mysql_client = getClientMariaDb(config['mysql'])
 
-	# Connect to MongoDb
-	print('* Connecting to MongoDb')
-	# mongodb_client = getClientMongoDb(config['mongodb'])
+    # Connect to MongoDb
+    print('* Connecting to MongoDb')
+    # mongodb_client = getClientMongoDb(config['mongodb'])
 
-	# Initialize Tensorflow Hub
-	print('* Load TensorflowHub.%s' % tfs_name)
-	embed = hub.Module("https://tfhub.dev/google/%s/%s" % (tfs_name, tfs_version))
-	model = embed(["mozart"])
-	print(model)
-	return True
+    # Initialize Tensorflow Hub
+    print('* Load TensorflowHub.%s' % tfs_name)
+    embed = hub.Module("https://tfhub.dev/google/%s/%s" % (tfs_name, tfs_version))
 
-	# Begin the treatment
-	print('* Get the last documents')
-	items = getLastSpiderResults(mongodb_client, 1)
-	if len(items):
-		# Get the fist document
-		html = items[0]['html']
-		# Get the words
-		words = getExtractWords(html)
+    # features = {
+    #     'sentences': np.array([
+    #         ['The', 'fish', 'swimming', 'in', 'the', 'water'],
+    #         ['Sea', 'is', 'beautiful', '', '', '']
+    #     ])
+    # }
+    #
+    # embeddings = embed(tf.reshape(features["sentences"], shape=[-1]))
 
-	print('End')
+    words = ['cat', 'dog', 'fish']
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.tables_initializer())
+
+        embeddings = sess.run(embed(words))
+        print(embeddings)
+
+        for i, embeddings in enumerate(np.array(embeddings).tolist()):
+            print("Message: {}".format(words[i]))
+            print("Embedding size: {}".format(len(embeddings)))
+            embeddings_snippet = ", ".join(
+                (str(x) for x in embeddings[:3]))
+            print("Embedding: [{}, ...]\n".format(embeddings_snippet))
+
+    return True
+
+    # Begin the treatment
+    print('* Get the last documents')
+    items = getLastSpiderResults(mongodb_client, 1)
+    if len(items):
+        # Get the fist document
+        html = items[0]['html']
+        # Get the words
+        words = getExtractWords(html)
+
+    print('End')
 
 # Auto start
 if __name__ == "__main__":
-	main()
+    main()
